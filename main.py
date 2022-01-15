419609b8a38bf2ac6b175e54ef61632d484be4f2
Sidhak <97508883+Sidhak3810@users.noreply.github.com>
2022-01-15 17:06:38 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+#Imports
+import discord
+from discord.ext import commands
+#Client
+client = commands.Bot(command_prefix="!")
+#StartUp
+@client.event
+async def on_ready():
+  print("I am ready!")
+#Commands
+@client.command()
+async def ping(ctx):
+  latency = round(client.latency * 1000)
+  await ctx.reply(f"Pong! My ping from the server to Discord is `{latency}`ms")
+#Run
+client.run()
