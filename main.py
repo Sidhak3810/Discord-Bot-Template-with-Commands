8b95a0cb616f32b1dbb8277b493d29910859f6ac
Sidhak <97508883+Sidhak3810@users.noreply.github.com>
2022-01-15 13:55:15 +0000

    Added a new command "Inspire"

--- main.py
+++ main.py
@@ -2,8 +2,16 @@
 import discord
 from discord.ext import commands
 import os
-#Variables
+import json
+import requests
+#Definations
 token = os.environ['TOKEN']
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a'] +"   " + "Quotes given by https://zenquotes.io"
+  return(quote)
+  
 #Client
 client = commands.Bot(command_prefix="!")
 #StartUp
@@ -15,5 +23,9 @@ async def on_ready():
 async def ping(ctx):
   latency = round(client.latency * 1000)
   await ctx.reply(f"Pong! My ping from the server to Discord is `{latency}`ms")
+@client.command()
+async def inspire(ctx):
+  quote = get_quote()
+  await ctx.reply(quote)
 #Run
 client.run(token)
